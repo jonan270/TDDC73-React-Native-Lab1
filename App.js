c57fd906ed07bb05adb90e3ec196f7b49a09731c
jonan270 <ormskagg@gmail.com>
2022-11-08 15:33:25 +0100

    Start implementation of react native application

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Email:</Text>
+      <Button
+        title="BUTTON"
+        color="#999999"
+        onPress={() => Alert.alert("THE BUTTON WAS PRESSED!")}
+      />
+      <Button
+        title="BUTTON"
+        color="#999999"
+        onPress={() => Alert.alert("THE BUTTON WAS PRESSED!")}
+      />
+      <Button
+        title="BUTTON"
+        color="#999999"
+        onPress={() => Alert.alert("THE BUTTON WAS PRESSED!")}
+      />
+      <Button
+        title="BUTTON"
+        color="#999999"
+        onPress={() => Alert.alert("THE BUTTON WAS PRESSED!")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +38,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
